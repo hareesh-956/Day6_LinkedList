1426a005dc32f0dc7dd140e04dab568341d9d4c1
hareesh <hareesh.vanapalli.888@gmail.com>
2023-10-08 18:55:50 +0530

    [hari]

---
+++ Day6_LinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Day6_LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            SimpleLinkedList myList = new SimpleLinkedList();
+            myList.AddNode(56);
+            myList.AddNode(30);
+            myList.AddNode(70);
+
+            Console.WriteLine("Linked List: ");
+            myList.DisplayList();
+        }
+    }
+}
---
+++ Day6_LinkedList/SimpleLinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_LinkedList
+{
+    internal class SimpleLinkedList
+    {
+        public Node Head;
+
+        public SimpleLinkedList()
+        {
+            Head = null;
+        }
+
+        public void AddNode(int data)
+        {
+            Node newNode = new Node(data);
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void DisplayList()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
